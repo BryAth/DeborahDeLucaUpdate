c70524c3ee16ab53598787e708a9d35815a377e6
BryAth <lequeux.bryan95@gmail.com>
2023-05-08 23:30:09 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log(gsap)
+
+gsap.fromTo("#d",{x:-400 , opacity : 0},{x:0,opacity:1,duration : 0.8})
+
+
+gsap.fromTo("#upperListe",
+{
+    x:1500,
+    opacity : 0
+}
+,
+{
+    x:0,
+    opacity : 1 , 
+    duration : 0.8
+})
